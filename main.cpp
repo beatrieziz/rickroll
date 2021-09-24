3553274e666d56f7950c324d9fc7924c18042ac1
beatrieziz <90811738+beatrieziz@users.noreply.github.com>
2021-09-24 11:20:38 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define never int
+#define gonna main
+#define give (
+#define you )
+#define up {
+#define Never std
+#define Gonna ::
+#define let cout
+#define You <<
+#define down "https://youtu.be/dQw4w9WgXcQ\n"
+#define nEver ;
+#define gOnna ;
+#define run ;
+#define around return
+#define And 0
+#define desert ;
+#define yOu }
+
+never gonna give you up
+Never Gonna let You down
+nEver gOnna run around
+And desert yOu
